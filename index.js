310fb7246a6857cfabd3e209c66c9a23a7ea6e0f
Mike Davis <mike@michaeljacobdavis.com>
2015-03-29 12:29:04 -0700

    Added schoolhouse

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 var Hapi = require('hapi');
+var Path = require('path');
+var Quilts = require('./data-store/quilts.json');
 
 var server = new Hapi.Server();
-server.connection({ port: process.env.PORT || 8080 });
+server.connection({ port: process.env.PORT || 9000 });
+
+server.route({
+    method: 'GET',
+    path: '/quilts/{name*}',
+    handler: function (request, reply) {
+        debugger;
+        reply.view('index', Quilts[request.params.name]);
+    }
+});
 
 server.route({
     path: "/{path*}",
     method: "GET",
     handler: {
         directory: {
-            path: "./public",
+            path: Path.join(__dirname, 'public'),
             listing: false
         }
     }
+});
+
+server.views({
+    engines: {
+        hbs: require('handlebars')
+    },
+    path: Path.join(__dirname, 'templates')
 });
 
 server.start(function () {
