84abaf4b0ad6684a457d64f7b7476e4b2be5a875
lorenmdavis <loren1660@gmail.com>
2016-12-13 20:01:02 -0800

    Updated to new design

--- index.js
+++ index.js
@@ -1,23 +1,8 @@
 var Hapi = require('hapi');
 var Path = require('path');
-var Quilts = require('./data-store/quilts.json');
 
 var server = new Hapi.Server();
 server.connection({ port: process.env.PORT || 9000 });
-
-server.route({
-    method: 'GET',
-    path: '/quilts/{name*}',
-    handler: function (request, reply) {
-        var quilt = Quilts[request.params.name];
-
-        if(quilt) {
-            return reply.view('index', quilt);
-        }
-
-        return reply('No quilt found.').code(404);
-    }
-});
 
 server.route({
     path: "/{path*}",
@@ -28,13 +13,6 @@ server.route({
             listing: false
         }
     }
-});
-
-server.views({
-    engines: {
-        hbs: require('handlebars')
-    },
-    path: Path.join(__dirname, 'templates')
 });
 
 server.start(function () {
